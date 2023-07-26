4cc0604e50827d65aab3f4762b0aeb5e98b850f0
DreamTexX <kevin.cebulla@dreamtexx.email>
2023-07-26 07:51:50 +0000

    feat(cli, server): basic connection handling, cli args and subcommands (#2)
    * feat(server): accept and handle client connections
    
    * feat(cli): cli args and subcommands
    
    Args:
    - MINECRUST_LOG, --log, -l > Log Level, default Info
    - MINECRUST_ADDR, --addr, -A> Server address, default 127.0.0.1:25565
    
    Subcommands:
    - server > start the server

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use clap::{Arg, ArgAction, Command, value_parser};
+use clap::builder::PossibleValue;
+use env_logger::Env;
+use server::ServerError;
+
+fn main() -> anyhow::Result<()> {
+    let matches = Command::new("minecrust")
+        .arg(
+            Arg::new("addr")
+                .env("MINECRUST_ADDR")
+                .short('A')
+                .long("addr")
+                .action(ArgAction::Set)
+                .value_name("IP:PORT")
+                .default_value("127.0.0.1:25565")
+                .value_parser(value_parser!(SocketAddr))
+                .help("IP address and port to bind to")
+                .global(true)
+        )
+        .arg(
+            Arg::new("log")
+                .env("MINECRUST_LOG")
+                .short('l')
+                .long("log")
+                .action(ArgAction::Set)
+                .value_name("LEVEL")
+                .default_value("info")
+                .value_parser([
+                    PossibleValue::new("trace"),
+                    PossibleValue::new("debug"),
+                    PossibleValue::new("info"),
+                    PossibleValue::new("warn"),
+                    PossibleValue::new("error"),
+                ])
+                .help("Log level")
+                .global(true)
+        )
+        .arg_required_else_help(true)
+        .subcommand(Command::new("server"))
+        .get_matches();
+
+    let log_level: &String = matches.get_one("log").unwrap();
+    env_logger::init_from_env(Env::new()
+        .filter("MINECRUST_LOG")
+        .default_filter_or(log_level)
+    );
+    log::info!("Logger initialized with level {}", log_level);
+
+    match matches.subcommand() {
+        Some(("server", args)) => {
+            let host: &SocketAddr = args.get_one("addr").unwrap();
+
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()?
+                .block_on(async {
+                    server::start(host).await?;
+
+                    Ok::<_, ServerError>(())
+                })?;
+        }
+        _ => {}
+    }
+    Ok(())
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,5 +1,9 @@
+use std::io::Error;
+use std::net::SocketAddr;
 use thiserror::Error;
-use tokio::net::{TcpListener, ToSocketAddrs};
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
+use tokio::spawn;
 
 #[derive(Error, Debug)]
 pub enum ServerError {
@@ -8,8 +12,49 @@ pub enum ServerError {
 
 }
 
-async fn start<A: ToSocketAddrs>(addr: A) -> Result<(), ServerError> {
-    let listener = TcpListener::bind(addr).await.map_err(|e| ServerError::InternalError(e))?;
+impl From<tokio::io::Error> for ServerError {
+    fn from(error: Error) -> Self {
+        Self::InternalError(error)
+    }
+}
 
+#[derive(Error, Debug)]
+pub enum ClientError {
+    #[error("internal error: {0}")]
+    InternalError(tokio::io::Error),
+}
+
+impl From<tokio::io::Error> for ClientError {
+    fn from(error: Error) -> Self {
+        Self::InternalError(error)
+    }
+}
+
+async fn handle_client(mut socket: TcpStream, _addr: SocketAddr) -> Result<(), ClientError> {
+    socket.flush().await?;
+    socket.shutdown().await?;
     Ok(())
+}
+
+pub async fn start<A: ToSocketAddrs>(addr: A) -> Result<(), ServerError> {
+    let listener = TcpListener::bind(addr).await?;
+    log::info!("Listening on {}...", listener.local_addr()?);
+
+    loop {
+        match listener.accept().await {
+            Ok((socket, addr)) => {
+                spawn(async move {
+                    log::info!("Client {} connected", addr);
+                    if let Err(e) = handle_client(socket, addr).await {
+                        log::error!("Client {} errored: {}", addr, e);
+                    } else {
+                        log::info!("Client {} disconnected", addr);
+                    }
+                });
+            }
+            Err(e) => {
+                log::error!("Client connection errored: {}", e);
+            }
+        }
+    }
 }
